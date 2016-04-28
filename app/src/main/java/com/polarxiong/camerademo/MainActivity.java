d5819817ca28a0ff99c7ba90eda5eb87819924d1
zhantong1994 <zhantong1994@163.com>
2016-04-28 19:18:50 +0800

    增加layout 现在有view了

--- app/src/main/java/com/polarxiong/camerademo/MainActivity.java
+++ app/src/main/java/com/polarxiong/camerademo/MainActivity.java
@@ -9,5 +9,6 @@ import android.os.Bundle;
 public class MainActivity extends Activity{
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
     }
 }
