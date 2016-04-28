f6a12b89191f8c6abae69defc015602861acb0d6
zhantong1994 <zhantong1994@163.com>
2016-04-28 13:49:26 +0800

    添加MainActivity，程序可以运行 layout中没有文件，不加载view

---
+++ app/src/main/java/com/polarxiong/camerademo/MainActivity.java
@@ -0,0 +1,13 @@
+package com.polarxiong.camerademo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by zhantong on 16/4/28.
+ */
+public class MainActivity extends Activity{
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+    }
+}
