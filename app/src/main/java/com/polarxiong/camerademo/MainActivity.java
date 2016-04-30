800b539deac0fc7e80b96581f1e0da0a5148dfb0
zhantong1994 <zhantong1994@163.com>
2016-04-30 20:53:42 +0800

    增加预览 可以相机预览了

--- app/src/main/java/com/polarxiong/camerademo/MainActivity.java
+++ app/src/main/java/com/polarxiong/camerademo/MainActivity.java
@@ -2,6 +2,7 @@ package com.polarxiong.camerademo;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.FrameLayout;
 
 /**
  * Created by zhantong on 16/4/28.
@@ -10,5 +11,9 @@ public class MainActivity extends Activity{
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CameraPreview mPreview=new CameraPreview(this);
+        FrameLayout preview=(FrameLayout)findViewById(R.id.camera_preview);
+        preview.addView(mPreview);
     }
 }
